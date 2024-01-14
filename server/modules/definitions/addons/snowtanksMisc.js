c8e5a8cdf0412f177d6e30ee2c9e6d8d24735179
LORD Arras <43840195+LORDCoder9@users.noreply.github.com>
2024-01-14 12:25:04 +0700

    Initial commit

---
+++ server/modules/definitions/addons/snowtanksMisc.js
@@ -0,0 +1,17 @@
+Class.towerBase = {
+    PARENT: 'wall',
+    LABEL: "Tower",
+    LAYER: 100,
+}
+Class.towerShort = {
+    PARENT: 'towerBase',
+    HEIGHT_SCALE: 1.5,
+}
+Class.towerMedium = {
+    PARENT: 'towerBase',
+    HEIGHT_SCALE: 2,
+}
+Class.towerTall = {
+    PARENT: 'towerBase',
+    HEIGHT_SCALE: 2.5,
+}
